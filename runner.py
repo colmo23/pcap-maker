519259969d9a99d796793b493f94368e06f6095d
Colm O'Shea <colm.oshea@adaptivemobile.com>
2021-11-14 17:16:04 +0000

    add web form

--- runner.py
+++ runner.py
@@ -1,7 +1,24 @@
-from bottle import route, run
+from bottle import route, run, get, post, request
+import pcap_utils
 
-@route('/hello')
-def hello():
-    return "Hello World!"
+
+@get('/') # or @route('/login')
+def get_network_info():
+    return '''
+        <form action="/" method="post">
+            dest port: <input name="dport" type="text" />
+            tcp hex data: <input name="tcphex" type="text" />
+            <input value="Generate PCAP" type="submit" />
+        </form>
+    '''
+
+@post('/') # or @route('/login', method='POST')
+def do_pcap():
+    dport = request.forms.get('dport')
+    dport = int(dport)
+    tcp_hex = request.forms.get('tcphex')
+    pcap_obj = pcap_utils.get_tcp_stack(tcp_data = tcp_hex, tcp_dest_port = dport)
+    
+    return "<p>Your information was %s - %s - %s</p>" % (dport, tcp_hex, repr(str(pcap_obj)))
 
 run(host='localhost', port=8080, debug=True)
