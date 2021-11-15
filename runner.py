eb315fec879c2dd4428472026022cda61d59c3ab
Colm O'Shea <colm.oshea@adaptivemobile.com>
2021-11-15 09:17:45 +0000

    downloading pcap file

--- runner.py
+++ runner.py
@@ -1,24 +1,30 @@
-from bottle import route, run, get, post, request
+from bottle import route, run, get, post, request, redirect, response
 import pcap_utils
 
+@route('/<path>')
+def all(path):
+    redirect("/")
 
-@get('/') # or @route('/login')
+@get('/') 
 def get_network_info():
     return '''
         <form action="/" method="post">
             dest port: <input name="dport" type="text" />
-            tcp hex data: <input name="tcphex" type="text" />
+            tcp hex data: <input name="tcphex" type="textarea" />
             <input value="Generate PCAP" type="submit" />
         </form>
     '''
 
-@post('/') # or @route('/login', method='POST')
+@post('/')
 def do_pcap():
     dport = request.forms.get('dport')
     dport = int(dport)
     tcp_hex = request.forms.get('tcphex')
-    pcap_obj = pcap_utils.get_tcp_stack(tcp_data = tcp_hex, tcp_dest_port = dport)
+    pkt = pcap_utils.get_tcp_stack(tcp_data = tcp_hex, tcp_dest_port = dport)
+    pcap_obj = pcap_utils.make_pcap(pkt)
+    response.content_type = 'application/cap'
+    response.set_header("Content-Disposition", 'attachment; filename="x.pcap"')
     
-    return "<p>Your information was %s - %s - %s</p>" % (dport, tcp_hex, repr(str(pcap_obj)))
+    return str(pcap_obj)
 
 run(host='localhost', port=8080, debug=True)
