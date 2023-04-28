6cc5a17eafa556b0366682edbf6fac69c1ac7cf3
Colm O'Shea <colm.oshea@adaptivemobile.com>
2023-04-28 15:31:29 +0100

    add gtp example

--- pcap_maker/html_pieces.py
+++ pcap_maker/html_pieces.py
@@ -232,6 +232,16 @@ FORM_UDP = '''
   </div>
   <div class="row">
     <div class="col-25">
+      GTP v1 Create PDP Context Request - smaller example (use dest port of 2123)
+    </div>
+    <div class="col-75">
+      <div class="wrapdiv">
+         3210001c000000009dbc00000215807922060357f00ffc108513c02a
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
       DNS request (use dest port of 53)
     </div>
     <div class="col-75">
